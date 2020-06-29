adad9c8f51cb4aaf3795ad33104907164a770ace
MatheusVictorCordeiro <matheus.cordeiro31@gmail.com>
2020-06-29 10:19:23 -0300

    metodo http post do Seller. criando um Seller e salvando no banco

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc2.Models;
 using SalesWebMvc2.Services;
 
 namespace SalesWebMvc2.Controllers
@@ -22,5 +23,30 @@ namespace SalesWebMvc2.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        //vai para a pagina Create.cshtml metodo get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //para dizer que é o metodo post
+        [HttpPost]
+        //para prevenir ataques csrf - aproveita a sessaão de autenticação para enviar dados maliliosos
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+
+        {
+
+            // chamamos o service para salvar no banco de dados, e depois retornamos para a view index para mostrar a lista de vendendores.
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+
+
+                }
+
+
+
     }
 }
